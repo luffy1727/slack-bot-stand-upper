6ebb1de9d7f166b8c7868dd166d870f6a4566385
luffy1727 <tushig.tushig@gmail.com>
2022-02-26 21:52:24 +0100

    Adding jira integration

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ My team requires us to write our plan for the day in slack  every morning around
 - [x] `Acquire oauth token from slack`
 - [x] Find out how to send messages to a slack channel as a user(- https://api.slack.com/methods/chat.postMessage). API doc etc 
 - [x] Try to use slack scheduled message? idk if it is necessary though. 
-- [ ] Fetch task list from Jira/Phabricator?.
-- [ ] `Set up AWS Lambda.`
+- [x] `Fetch task list from Jira/Phabricator?.`
+- [x] `Set up AWS Lambda.`
+- [ ] `Add date checker? (if tuesday or thursday add WFH at the end.)`
 
 # BACKLOG:
 
@@ -21,3 +22,9 @@ My team requires us to write our plan for the day in slack  every morning around
 - Do i have to upload json file every week or even everyday?
 - In order to get tasks form phabricator i need to create an Oauth server(which i don't have access for) or actually crawl the web(i don't know how to get past the 2fa).
 (https://developer.atlassian.com/server/jira/platform/rest-apis/)
+
+https://yourdomain.com/rest/api/2/search?jql=status+in+%28%22In+progress%22%2C+%22To+do%22%2C+done%29+order+by+status
+
+https://developer.atlassian.com/cloud/jira/platform/basic-auth-for-rest-apis/
+
+useremail:api-token gedgee base64 ntr bolgoh yum bn
--- index.js
+++ index.js
@@ -1,32 +1,95 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 const https = require('https');
-const taskData = require('./data/tasks.json');
+const readyTasks = [];
+const inProgressTasks = [];
 const greetingsMessages = ['Good morning! ', 'Hello, ', 'Greetings! ', 'Morning! '];
 
 dotenv.config();
 
 const slackToken = process.env.SLACK_TOKEN;
 const slackChannel = process.env.SLACK_CHANNEL_ID;
+const apiToken = process.env.ATLASSIAN_API_TOKEN;
+const email = process.env.ATLASSIAN_USER_EMAIL;
+const jiraToken = Buffer.from(email + ":" + apiToken).toString('base64');
 
 console.log('Starting...');
 
+function getTasks() {
+    var options = {
+        hostname: 'boozt.atlassian.net',
+        path: '/rest/api/2/search?jql=assignee=61927ce4744c4d006936cda3',
+        method: 'GET',
+        port: 443,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + jiraToken,
+        }
+      };
+    
+      var req = https.request(options, (res) => {
+        if (res.statusCode != 200) {
+          console.log('ERROR');
+        }
+        console.log('statusCode:', res.statusCode);
+        var body = [];
+        res.on('data', (d) => {
+          body.push(d);
+        });
+
+        res.on('end', function() {
+            try {
+                body = JSON.parse(Buffer.concat(body).toString());
+            } catch(e) {
+                console.log(e);
+            }
+            parseTasks(body);
+            sendMessage();
+            console.log(inProgressTasks);
+            console.log(readyTasks);
+        });
+
+      });
+      
+      req.on('error', (e) => {
+        console.error(e);
+      });
+      
+      req.end();
+}
+
+function parseTasks(data) {
+  data.issues.forEach(task => {
+    if (task.fields.status.statusCategory.id == 4 &&
+      task.fields.status.name == 'In Progress') {
+      console.log(task.fields.status);
+      inProgressTasks.push(task.fields.summary)
+    } else if (task.fields.status.statusCategory.id == 2) {
+      readyTasks.push(task.fields.summary)
+    }
+  })
+}
+
+getTasks();
+
 function buildMessage() {
     const random = Math.floor(Math.random() * greetingsMessages.length);
-    const taskMessage = taskData['in-progress'].length < 1 
-                        ? 'I haven`t decided what to work on today, but I am alive and well.' +
-                        ' I will decide what to work on, once i get to work.' : 'I will continue to work on ' + taskData['in-progress'][0].name
+    const random2 = Math.floor(Math.random() * readyTasks.length);
+    const taskMessage = inProgressTasks.length < 1 
+                        ? 'I will start working on "' + readyTasks[0] + '".'
+                         : 'I will continue to work on "' + inProgressTasks[0]  + '".'
     const message = greetingsMessages[random] + taskMessage
     
     return message
 }
 
-var postData = JSON.stringify({
+function sendMessage() {
+  var postData = JSON.stringify({
     'channel' : slackChannel,
     'text' : buildMessage(),
-});
+  });
 
-var options = {
+  var options = {
     hostname: 'slack.com',
     path: '/api/chat.postMessage',
     method: 'POST',
@@ -51,3 +114,4 @@ var options = {
   
   req.write(postData);
   req.end();
+}
